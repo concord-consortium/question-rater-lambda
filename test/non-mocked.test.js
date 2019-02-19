fb4b34dc5c158f787a69393579b3cbe9200926d4
Doug Martin <dmartin@concord.org>
2019-02-19 14:39:51 -0500

    Added non-mocked test of rater endpoint

---
+++ test/non-mocked.test.js
@@ -0,0 +1,32 @@
+const {handler} = require("../index");
+const {assertXmlMatch} = require("./test-utils");
+
+test("returns a valid xml response on a good request to the non-mocked service", async () => {
+  const event = {
+    headers: {"Content-Type": "text/html"},
+    body: "<crater-request includeRNS=\"N\">\n\t<client id=\"cc\"/>\n\t<items>\n\t  <item id=\"123\">\n\t    <responses>\n\t      <response id=\"456\">\n\t        <![CDATA[test]]>\n\t      </response>\n\t    </responses>\n\t  </item>\n\t</items>\n</crater-request>"
+  };
+
+  // see https://github.com/concord-consortium/lara/blob/master/spec/libs/c_rater/api_wrapper_spec.rb#L30
+  const result = await handler(event);
+  const client_id = "cc";
+  const item_id = "123";
+  const response_id = "456";
+  const score = "1";
+  expect(result.statusCode).toEqual(200);
+  expect(result.headers['Content-Type']).toEqual('text/xml');
+  const expectedXml = `
+    <crater-results>
+      <tracking id="12345"/>
+      <client id="${client_id}"/>
+      <items>
+        <item id="${item_id}">
+          <responses>
+            <response id="${response_id}" score="${score}"/>
+          </responses>
+        </item>
+      </items>
+    </crater-results>
+  `;
+  assertXmlMatch(result.body, expectedXml);
+});
