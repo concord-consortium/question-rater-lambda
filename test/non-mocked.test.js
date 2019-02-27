5f4b9f569eec4a96fa46cf9afb9949cd55c96931
Doug Martin <dmartin@concord.org>
2019-02-27 15:23:00 -0500

    Added non-mocked test without authorization header [#164120630]

--- test/non-mocked.test.js
+++ test/non-mocked.test.js
@@ -1,11 +1,37 @@
 const {handler} = require("../index");
-const {assertXmlMatch} = require("./test-utils");
-const { authorizationValue } = require("../auth");
+const {assertXmlMatch, errorResult} = require("./test-utils");
+const {authorizationValue} = require("../auth");
 
 const headers = {
   "Content-Type": "text/html",
   Authorization: authorizationValue
 };
+const headersWithoutAuthorization = {
+  "Content-Type": "text/html"
+};
+
+test("returns an error when no authorization header is present", async () => {
+  const event = {
+    headers: headersWithoutAuthorization,
+    body: `
+      <crater-request includeRNS="N">
+        <client id="cc"/>
+          <items>
+            <item id="FUTURE_X">
+              <responses>
+                <response id="456">
+                  <![CDATA[test]]>
+                </response>
+              </responses>
+            </item>
+          </items>
+        </crater-request>`
+  };
+
+  const result = await handler(event);
+  expect(result.statusCode).toEqual(400);
+  expect(result).toEqual(errorResult("Error: Missing Authorization header!"));
+});
 
 test("returns a valid xml response on a good request to the non-mocked automl FUTURE_X service", async () => {
   const event = {
